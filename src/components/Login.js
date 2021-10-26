f9e99dc48eeb69fbf68d2a701fb190ba3a78f83a
“abstross” <abstross@pm.me>
2021-10-26 16:15:27 -0500

    converted login to hooks

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,41 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import axios from 'axios'
+//import history from './../history'
 
-class Login extends React.Component {
-  handleLogin = (e) => {
-    e.preventDefault()
-    axios({
-      method: 'POST',
-      url: 'http://localhost:3001/auth/sign_in',
-      data: {
-        email: this.email.value,
-        password: this.password.value
+function Login(props){
+  const [error, setError] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+
+  const handleLogin = useCallback(
+    event => {
+      event.preventDefault();
+      setEmail(event.target.elements.email.value)
+      setPassword(event.target.elements.password.value)
+    },
+    [setEmail,setPassword]
+  );
+
+    useEffect((e) => {
+         if (email && password) {
+           axios({
+             method: 'POST',
+             url: 'http://localhost:3001/auth/sign_in',
+             data: {
+               email: email,
+               password: password
+             }
+           })
+           .then((response) => {
+             console.log(response)
+             setError(undefined);
+             localStorage.setItem('user',
+             JSON.stringify({
+               'access-token': response.headers['access-token'],
+               'client': response.headers['client'],
+               'uid': response.data.data.uid
+             }))
+             window.location = '/'
+           })
+      .catch(error => {
+        console.log(error)
+        setEmail();
+        setPassword();
+        setError(error);
+        })
       }
-    })
-    .then(response => {
-      console.log(response)
-      localStorage.setItem('user',
-        JSON.stringify({
-          'access-token': response.headers['access-token'],
-          'client': response.headers['client'],
-          'uid': response.data.data.uid
-      }))
-      window.location = '/'
-    })
-  }
+    }, [error,email,password])
 
-  render () {
+
     return (
       <div>
         <h2>Log in</h2>
-        <form onSubmit={this.handleLogin} >
-          <input name="email" ref={(input) => this.email = input } />
-          <input name="password" type="password" ref={(input) => this.password = input } />
+        <form onSubmit={handleLogin} >
+          <input name="email" />
+          <input name="password" type="password"  />
           <input type="submit" value="Log in" />
         </form>
       </div>
     )
-  }
 }
 
 export default Login
